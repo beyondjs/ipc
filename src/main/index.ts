128f3130f1bf5a7ed193aaebfeee0016298fb2b6
Henry Box <14365005+henrybox@users.noreply.github.com>
2025-08-02 18:44:33 -0300

    fix: export /*bundle*/ ipc

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
 import Main from './main';
 import Child from './child';
 
-const ipc = process.send ? new Child() : new Main();
+export /*bundle*/ const ipc = process.send ? new Child() : new Main();
